d8d72da456c081754c11c41a6aa9b4efaecc47b9
Hana-Workneh <hanandarge19@gmail.com>
2023-11-04 03:22:41 +0300

    back navigation added

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,4 +1,13 @@
 <template>
+  <div>
+    <!-- Your login form content here -->
+    <router-link to="/maincontent"></router-link>
+  </div>
+  <button @click="goBack"
+  class="absolute top-0 left-0 m-2 px-4 py-2  bg-blue-500 text-white rounded-lg hover:bg-blue-700"
+>
+  Back
+</button>
   <div class="max-w-sm mx-auto mt-8 p-4 bg-white text-gray-800 rounded-lg shadow-lg">
     <h2 class="text-2xl font-semibold mb-4">Login</h2>
     <form @submit.prevent="loginUser">
@@ -31,6 +40,11 @@
     },
 
     methods: {
+    goBack() {
+      this.$router.go(-1); // Go back to the previous page but with vue routing
+      // Use JavaScript's built-in history to navigate back
+      // window.history.go(-1);
+    },
     loginUser() {
       axios.post('https://hedonic-backend.onrender.com/user/login', {
         email: this.email,
